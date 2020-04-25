f760203b41dd26bac6ef7197aa085bfcd97e7b9e
FKouhai <frandres00@gmail.com>
2020-04-26 00:12:43 +0200

    Update añadirUsus.ps1

--- añadirUsus.ps1
+++ añadirUsus.ps1
@@ -1,8 +1,14 @@
-﻿Write-Host "Para usar este script solo se necesita un fichero csv con el user id y el nombre del grupo"
+Write-Host "Para usar este script solo se necesita un fichero csv con el user id y el nombre del grupo"
 
 $ruta=Read-Host "Introduce la ruta del fichero csv(fichero que contiene los usuarios)"
 $fichero= Import-csv -Path $ruta
 $grupo=Read-host "Introduce el nombre de los grupos de seguridad"
+
+
 foreach($user in $fichero){
-    Add-ADGroupMember -Identity $group  -Members $user  
+    if (Get-ADPrincipalGroupMembership $usuario|select name | Where-Object name -ne $grupo -eq $true){
+        Add-ADGroupMember -Identity $grupo  -Members $user  
+}else{
+Write-Host "El usuario ya esta incluido en el grupo de seguridad"
+}
 }
