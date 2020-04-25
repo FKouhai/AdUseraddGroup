7eb67a470f81358b37fada5eb65ae925f6aeb674
FKouhai <frandres00@gmail.com>
2020-04-25 23:39:59 +0200

    Add files via upload

---
+++ añadirUsus.ps1
@@ -0,0 +1,8 @@
+﻿Write-Host "Para usar este script solo se necesita un fichero csv con el user id y el nombre del grupo"
+
+$ruta=Read-Host "Introduce la ruta del fichero csv(fichero que contiene los usuarios)"
+$fichero= Import-csv -Path $ruta
+$grupo=Read-host "Introduce el nombre de los grupos de seguridad"
+foreach($user in $fichero){
+    Add-ADGroupMember -Identity $user  -Members $user  
+}
