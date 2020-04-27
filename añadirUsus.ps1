048e2c2f4db0700de388bb993d63b83efff226bc
FKouhai <frandres00@gmail.com>
2020-04-27 21:05:30 +0200

    Update añadirUsus.ps1

--- añadirUsus.ps1
+++ añadirUsus.ps1
@@ -1,9 +1,9 @@
 Write-Host "Para usar este script solo se necesita un fichero csv con el user id y el nombre del grupo"
 function vars{
-$ruta=Read-Host "Introduce la ruta del fichero csv(fichero que contiene los usuarios)"
-$fichero= Import-Csv -Path $ruta
-$grupo=Read-host "Introduce el nombre de los grupos de seguridad"
-}
+    $ruta=Read-Host "Introduce la ruta del fichero csv(fichero que contiene los usuarios)"
+    $fichero= Import-Csv -Path $ruta
+    $grupo=Read-host "Introduce el nombre de los grupos de seguridad"
+    }
 vars
 
 if(Get-ADGroup -Identity $grupo){
@@ -22,20 +22,15 @@ else{
 #Se comprueba que los usuarios 
 #No estan incluidos en el grupo de AD
 function añadir {
-foreach($user in $fichero){
-    if (Get-ADPrincipalGroupMembership $usuario|select name | Where-Object name -ne $grupo){
+    foreach($user in $fichero){
+        if (Get-ADPrincipalGroupMembership $usuario|select name | Where-Object name -ne $grupo){
         Add-ADGroupMember -Identity $grupo -Members $user  
-}
-    else{
-    Write-Host "El usuario $user ya esta incluido en el grupo de seguridad"
+        }
+        else{
+        Write-Host "El usuario $user ya esta incluido en el grupo de seguridad"
+        }
     }
 }
-}
-Write-Host "Para usar este script solo se necesita un fichero csv con el user id y el nombre del grupo"
-function vars{
-$ruta=Read-Host "Introduce la ruta del fichero csv(fichero que contiene los usuarios)"
-$fichero= Import-Csv -Path $ruta
-$grupo=Read-host "Introduce el nombre de los grupos de seguridad"
-}
-vars
+
+
 
