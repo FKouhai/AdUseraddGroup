174d373819d232c14fe42615710dcebee07f3d1d
FKouhai <frandres00@gmail.com>
2020-04-27 20:53:41 +0200

    Update añadirUsus.ps1

--- añadirUsus.ps1
+++ añadirUsus.ps1
@@ -39,28 +39,3 @@ $grupo=Read-host "Introduce el nombre de los grupos de seguridad"
 }
 vars
 
-if(Get-ADGroup -Identity $grupo){
-    Write-Host "El grupo de AD existe"
-    añadir
-}
-else{
-    Write-Host "El grupo de AD no existe"
-    #Llamamos a la funcion vars para volver a preguntar 
-    #Por el grupo de AD
-    vars
-}
-
-
-#Funcion para añadir a los usuarios al grupo de AD
-#Se comprueba que los usuarios 
-#No estan incluidos en el grupo de AD
-function añadir {
-foreach($user in $fichero){
-    if (Get-ADPrincipalGroupMembership $usuario|select name | Where-Object name -ne $grupo){
-        Add-ADGroupMember -Identity $grupo -Members $user  
-}
-    else{
-    Write-Host "El usuario $user ya esta incluido en el grupo de seguridad"
-    }
-}
-}
